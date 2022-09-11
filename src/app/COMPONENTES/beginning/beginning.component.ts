025313e450f44cd7b4ab739a62d45d4096adc0cb
jaimebp07 <andrescodigo07@gmail.com>
2022-09-11 09:55:01 -0500

    hasta el omento la aplicacion muestra la lista de clientes

---
+++ src/app/COMPONENTES/beginning/beginning.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beginning',
+  templateUrl: './beginning.component.html',
+  styleUrls: ['./beginning.component.css']
+})
+export class BeginningComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BeginComponent } from './COMPONENTES/begin/begin.component';
+import { BeginningComponent } from './COMPONENTES/beginning/beginning.component';
+import { AddComponent } from './COMPONENTES/add/add.component';
+import { ModifyComponent } from './COMPONENTES/modify/modify.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeginComponent,
+    BeginningComponent,
+    AddComponent,
+    ModifyComponent
   ],
   imports: [
     BrowserModule
